58ec0c0e3ecfb2a5670606b30422573912094804
Alex <gearmand@yandex.ru>
2020-04-01 00:16:17 +0800

    first commit

---
+++ js/myscript.js
@@ -0,0 +1,10 @@
+function coffeeMachine(){
+    console.log("Select option");
+    coffeeMachine = function(){
+        console.log("Coffee machine breakes");
+    }
+}
+let message = coffeeMachine;
+coffeeMachine();
+coffeeMachine();
+message();
